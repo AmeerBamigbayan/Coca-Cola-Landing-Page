1c49ab1c564654ff907e99bf7f53d18f2c6cdbd8
Ameer_105 <ameerdamilola@gmail.com>
2023-03-04 06:50:59 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const scrollHeader = () =>{
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
     this.scrollY >= 50 ? header.classList.add('scroll-header') 
                        : header.classList.remove('scroll-header')
-}
+} 
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
